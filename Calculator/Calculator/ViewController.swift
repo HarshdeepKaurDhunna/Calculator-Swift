fb2f8d5a4f9289d5060e4e9afa36e696ae5b6890
HarshdeepKaurDhunna <97932988+HarshdeepKaurDhunna@users.noreply.github.com>
2022-02-26 16:06:25 -0500

    Create Actions for every single button

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -13,7 +13,71 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet weak var calculationArea: UILabel!
+    
+    @IBOutlet weak var calculationResult: UILabel!
+    
+    @IBAction func clearScreen(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func deleteFunc(_ sender: Any) {
+    }
+    
+    @IBAction func percentageFunc(_ sender: Any) {
+    }
+    
+    
+    @IBAction func modulusFunc(_ sender: Any) {
+    }
+    
+    
+    @IBAction func multiplyFunc(_ sender: Any) {
+    }
+    
+    @IBAction func minusFunc(_ sender: Any) {
+    }
+    
+    @IBAction func plusFunc(_ sender: Any) {
+    }
+    
+    @IBAction func equalFunc(_ sender: Any) {
+    }
+    
+    
+    @IBAction func calDigitNine(_ sender: Any) {
+    }
+    
+    @IBAction func calDigitEight(_ sender: Any) {
+    }
+    
+    @IBAction func calDigitSeven(_ sender: Any) {
+    }
 
+    @IBAction func calDigitSix(_ sender: Any) {
+    }
+    
+    @IBAction func calDigitFive(_ sender: Any) {
+    }
 
+    @IBAction func calDigitFour(_ sender: Any) {
+    }
+    
+    @IBAction func calDigitThree(_ sender: Any) {
+    }
+    
+    @IBAction func calDigitTwo(_ sender: Any) {
+    }
+    
+    @IBAction func calDigitOne(_ sender: Any) {
+    }
+    
+    @IBAction func calDigitZero(_ sender: Any) {
+    }
+    
+    @IBAction func calDigitDot(_ sender: Any) {
+    }
 }
 
